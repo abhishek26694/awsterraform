937ef147db95f58f235327772a1767981c24dcca
Abhishek Upadhyay <abhishek.upadhyay@zensar.com>
2022-10-25 11:17:30 +0530

    Updating new Resource

--- news3.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "testBucket" {
-  bucket = "testbucket-abhishek1"
-} 
-
-resource "aws_s3_bucket_acl" "testBucket_acl" {
-  bucket = aws_s3_bucket.testBucket.id
-  acl    = "private"
-}
